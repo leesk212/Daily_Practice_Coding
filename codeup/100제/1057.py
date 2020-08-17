ac411538c7f046706be1712db2da1dd5be509de6
leesk212@gmail.com <leesk212@gmail.com>
2020-08-17 18:21:01 +0900

    (논리연산 기타)  가끔은 그냥 if else로 해도 괜찮다.

---
+++ codeup/100제/1057.py
@@ -0,0 +1,6 @@
+first,second = map(int,input().split())
+
+if first == second:
+    print(1)
+else:
+    print(0)
---
+++ codeup/100제/1058.py
@@ -0,0 +1,6 @@
+first,second = map(int,input().split())
+
+if first | second == True:
+    print(0)
+else:
+    print(1)
