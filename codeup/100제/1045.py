2474d5dce86296e0fc52cdf98fb444f065f4d994
leesk212@gmail.com <leesk212@gmail.com>
2020-08-17 15:58:27 +0900

    (사칙연산+float 소수점 출력)

---
+++ codeup/100제/1045.py
@@ -0,0 +1,8 @@
+first,second = map(int,input().split())
+
+print(first+second)
+print(first-second)
+print(first*second)
+print(int(first/second))
+print(first%second)
+print('%.2f' %(first/second))
