1a74479d616cb5aa82360d8077271813f4551700
leesk212@gmail.com <leesk212@gmail.com>
2020-08-19 06:43:37 +0900

    (ABCD성적 출력)

---
+++ codeup/100제/1068.py
@@ -0,0 +1,13 @@
+data = int(input())
+
+if data <=100:
+    if data < 90:
+        if data <70:
+            if data <40:
+                print('D')
+            else:
+                print('C')
+        else:
+            print('B')
+    else:
+        print('A')
