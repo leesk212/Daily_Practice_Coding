732e36e8c7c3211815c4e82c4394b03e60121618
leesk212@gmail.com <leesk212@gmail.com>
2020-08-19 17:57:15 +0900

    (ABCD성적 출력)

---
+++ codeup/100제/1069.py
@@ -0,0 +1,10 @@
+data = input()
+
+if data =='A':
+    print('best!!!')
+    if data =='B':
+        print('good!!')
+        if data =='C':
+            print('run!')
+            if data =='D':
+                print('slowly~')
