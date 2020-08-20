a73bce4beed985cddba415a3913bc7aa1615924b
leesk212@gmail.com <leesk212@gmail.com>
2020-08-20 10:09:51 +0900

    (dictionary by python)  dictionary형태의 python은 중복의 key값을 가질 수 없다. 튜플로 (1,2):'a'를 쓴다고해도, 키 값이 (1,2)가 될 뿐이다.
    Signed-off-by: leesk212@gmail.com <leesk212@gmail.com>

---
+++ codeup/100제/1070.py
@@ -0,0 +1,17 @@
+def f(x):
+    return {12:'winter',
+            1: 'winter',
+            2: 'winter',
+            3: 'spring',
+            4: 'spring',
+            5: 'spring',
+            6: 'summer',
+            7: 'summer',
+            8: 'summer',
+            9: 'fall',
+            10: 'fall',
+            11: 'fall'
+            }.get(x,'what?')
+data = int(input())
+print(f(data))
+
