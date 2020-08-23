dc843a08c7c12530ca31160723800387404a6bd9
leesk212@gmail.com <leesk212@gmail.com>
2020-08-24 02:30:35 +0900

    20/08/24
    Signed-off-by: leesk212@gmail.com <leesk212@gmail.com>

--- [Book]This_is_coding_Test/code/1_simulation_게임개발.py
+++ [Book]This_is_coding_Test/code/1_simulation_게임개발.py
@@ -1,9 +1,9 @@
 size_x, size_y = map(int, input().split())
-a,b,d = map(int,input().split())
+a, b, d = map(int, input().split())
 
-direction = ['0','3','2','1']
-move_x = [0,-1,0,1]
-move_y = [-1,0,1,0]
+direction = ['0', '3', '2', '1']
+move_x = [0, -1, 0, 1]
+move_y = [-1, 0, 1, 0]
 
 MAP = []
 
@@ -12,11 +12,24 @@ for i in range(size_x):
     MAP.append(temp)
 
 index = 0
+count = 0
+ans = 0
 n_a = int()
 n_b = int()
 while True:
+    for i in range(4):
+        n_a = a + move_x[i]
+        n_b = b + move_y[i]
+        if MAP[n_a][n_b] == 1 or MAP[n_a][n_b]:
+            count += 1
+    if count == 4:
+        print(ans)
+        break
+
     MAP[a][b] = 9
     index = index + 1
+    if index == 4:
+        index = 0
     d = direction[index]
     for i in range(len(direction)):
         if d == direction[i]:
@@ -24,7 +37,9 @@ while True:
             n_b = b + move_y[i]
 
     if MAP[a][b] != 0:
-       continue
+        continue
 
     a = n_a
     b = n_b
+
+    ans += 1
